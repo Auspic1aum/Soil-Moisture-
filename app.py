ce9be6b1575dc339d874dd96f6d3db2b7874586a
Auspic1aum <92164762+Auspic1aum@users.noreply.github.com>
2022-10-01 19:32:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from collections.abc import Mapping
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
